1d7e608e2df1211da45ed66d9ae8980e9b7039f5
nosianuri <nurinosia@gmail.com>
2023-02-21 02:21:25 +0600

    chart color

--- js/my_chart.js
+++ js/my_chart.js
@@ -6,13 +6,12 @@ var myChart = new Chart(ctx, {
         labels:['Send', 'Failed', 'Inprogress'],
         datasets: [{
             label: 'Traffic Source',
-            data: [1200, 1900, 3000],
+            data: [1500, 900, 3000],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
+                '#002F42',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'  
+                '#00C1C6'  
             ],
-            
         }]
     },
     options: {
@@ -27,18 +26,18 @@ var earning = new Chart(earning, {
             label: 'Rate of msg',
             data: [1200, 1900, 3000, 5000, 2000, 3000, 4800, 4578, 25566, 8545, 4587, 9000],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',  
-                'rgba(75, 192, 192, 1)',  
-                'rgba(153, 102, 255, 1)',  
-                'rgba(255, 159, 64, 1)',   
-                'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',  
-                'rgba(75, 192, 192, 1)',  
-                'rgba(153, 102, 255, 1)',  
-                'rgba(255, 159, 64, 1)'   
+                'rgba(54, 162, 235, 1)',  
+                '#00C1C6',  
+                'rgba(54, 162, 235, 1)',  
+                'rgba(54, 162, 235, 1)',   
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                '#002F42',  
+                '#00C1C6',  
+                'rgba(54, 162, 235, 1)',  
+                '#00C1C6'   
             ],
             borderWidth: 1
         }]
